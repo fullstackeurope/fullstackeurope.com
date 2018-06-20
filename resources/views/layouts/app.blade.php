0dfdd30c46548cfa29903fbd10224bd700dcd2b5
freek <freek@spatie.be>
2018-06-20 22:39:37 +0200

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,19 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name') }}</title>
+    <title>{{ config('app.name') }}</title>
 
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-        <script defer src="{{ mix('js/app.js') }}"></script>
-    </head>
-    <body>
-        {{ $slot }}
-    </body>
+    <script defer src="{{ mix('js/app.js') }}"></script>
+</head>
+<body>
+<div class="flex flex-col text-red-dark">
+    {{ $slot }}
+</div>
+</body>
 </html>

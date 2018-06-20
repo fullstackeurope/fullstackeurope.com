fba3a2b8d6784235dbf004d6edbadd8c11854ff0
freek <freek@spatie.be>
2018-06-20 22:12:14 +0200

    add title

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,5 +1,5 @@
 @component('layouts.app')
     <header class="flex items-center justify-center min-h-screen">
-        <span class="text-4xl">🥄</span>
+        <span class="text-4xl">Full Stack Europe</span>
     </header>
 @endcomponent

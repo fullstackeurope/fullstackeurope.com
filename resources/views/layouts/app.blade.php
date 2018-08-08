08fdf3cae3b97e01c4ffd597f183ad1e0fbadbdb
Dries Vints <dries.vints@gmail.com>
2018-08-08 23:10:24 +0200

    Add og tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,8 +32,28 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="/images/favicons/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
+    
+    <!-- HTML Meta Tags -->
+    <title>Full Stack Europe - A conference coming to Antwerp, Belgium in 2019</title>
+    <meta name="description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in 2019.">
 
-    <title>Full Stack Europe - An amazing conference coming to Antwerp, Belgium in 2019</title>
+    <!-- Google / Search Engine Tags -->
+    <meta itemprop="name" content="Full Stack Europe - A conference coming to Antwerp, Belgium in 2019">
+    <meta itemprop="description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in 2019.">
+    <meta itemprop="image" content="https://fullstackeurope.com/images/ogimage.png">
+
+    <!-- Facebook Meta Tags -->
+    <meta property="og:url" content="https://fullstackeurope.com">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Full Stack Europe - A conference coming to Antwerp, Belgium in 2019">
+    <meta property="og:description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in 2019.">
+    <meta property="og:image" content="https://fullstackeurope.com/images/ogimage.png">
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Full Stack Europe - A conference coming to Antwerp, Belgium in 2019">
+    <meta name="twitter:description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in 2019.">
+    <meta name="twitter:image" content="https://fullstackeurope.com/images/ogimage.png">
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">

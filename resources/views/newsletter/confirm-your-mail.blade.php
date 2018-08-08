b51872f2a8d59f972a451d0ea0ac9ec5900113fa
Dries Vints <dries.vints@gmail.com>
2018-08-08 21:14:03 +0200

    Copy

--- resources/views/newsletter/confirm-your-mail.blade.php
+++ resources/views/newsletter/confirm-your-mail.blade.php
@@ -2,7 +2,7 @@
     <h3 class="text-3xl mb-16">You’re almost there…</h3>
 
     <p class="text-xl mb-16 leading-normal">
-        Thanks for subscribing to our newsletter. There’s an e-mail on it’s way to your address for verification.
-        Just click the link to verify your address and you’ll enjoy receiving our newsletter.
+        Thanks for signing up! There’s an e-mail on it’s way to your address for verification.
+        Just click the link to verify your address and you're all set.
     </p>
 @endcomponent
--- resources/views/newsletter/subscribed.blade.php
+++ resources/views/newsletter/subscribed.blade.php
@@ -2,7 +2,7 @@
     <h3 class="text-3xl mb-16">You are now subscribed!</h3>
 
     <p class="text-xl mb-16 leading-normal">
-        Awesome! You are now subscribed to our newsletter.
+        Awesome! You are now fully signed up.
         We'll let you know as soon as we have more details about the conference.
     </p>
 @endcomponent

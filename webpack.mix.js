e01a24ab4a64708959604e00452ed31a5f76f3f5
Dries Vints <dries.vints@gmail.com>
2018-12-31 16:17:36 +0100

    Fresh Tailwindcss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
+let tailwindcss = require('tailwindcss');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
@@ -15,4 +16,6 @@ mix.js('source/_assets/js/main.js', 'js')
     .sass('source/_assets/sass/main.scss', 'css')
     .options({
         processCssUrls: false,
-    }).version();
+        postCss: [ tailwindcss('./tailwind.js') ],
+    })
+    .version();

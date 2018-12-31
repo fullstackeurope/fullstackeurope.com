679c0c5ad593d92bfb9fc42b9f7e61f8a9a9be67
Dries Vints <dries.vints@gmail.com>
2018-12-31 18:10:48 +0100

    Responsive adjusments

---
+++ source/_layouts/footer.blade.php
@@ -0,0 +1,42 @@
+<div class="footer text-sm">
+    <div class="footer-top leading-normal">
+        <div class="container pt-16 pb-8">
+            <div class="md:flex md:mb-4">
+                <div class="md:w-1/2 mb-12 md:mb-0">
+                    <img src="/assets/images/logo-white.png" class="mb-4 md:mb-6" width="150" alt="Full Stack Europe"><br>
+                    2 & 3 & 4 October 2019 - Venue Antwerp
+                </div>
+                <div class="md:w-1/2 md:text-right">
+                    <ul class="list-reset">
+                        <li class="md:inline-block mb-4"><a href="#">speakers</a></li>
+                        <li class="md:ml-6 md:inline-block mb-4"><a href="#">schedule</a></li>
+                        <li class="md:ml-6 md:inline-block mb-4"><a href="#">workshops</a></li>
+                        <li class="md:ml-6 md:inline-block mb-4"><a href="#">venue</a></li>
+                        <li class="md:ml-6 md:inline-block mb-4"><a href="#">sponsors</a></li>
+                        <li class="md:ml-6 md:inline-block mb-4"><a href="#">tickets</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom leading-loose text-center">
+        <div class="container pt-8 pb-8">
+            <div class="md:flex md:mb-4">
+                <div class="md:w-3/4 md:text-left">
+                    &copy; Full Stack Europe
+                    <span class="hidden md:inline-block">|</span>
+                    <span class="md:hidden"><br></span>
+                    Design by <a href="https://janvanechelpoel.be">Jan Van Echelpoel</a>
+                </div>
+                <div class="md:w-1/4 md:text-right text-lg">
+                    <a href="mailto:hello@fullstackeurope.com" class="mr-3">
+                        <i class="fas fa-envelope"></i>
+                    </a>
+                    <a href="https://twitter.com/fullstackeu">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

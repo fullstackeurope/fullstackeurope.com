ffb97521e79880ce8b5c660c1a55951241b481ef
Dries Vints <dries.vints@gmail.com>
2019-01-25 16:31:55 +0100

    Favicons

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -7,6 +7,8 @@
     <title>Full Stack Europe</title>
     <meta name="description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in 2019.">
 
+    @include('_layouts.favicons')
+
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
 </head>

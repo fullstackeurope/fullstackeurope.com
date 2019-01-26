f8c106594493ddc64329556616db59fb92f41f8b
Dries Vints <dries.vints@gmail.com>
2019-01-26 11:44:36 +0100

    Fix font size button

--- source/_layouts/header.blade.php
+++ source/_layouts/header.blade.php
@@ -25,7 +25,7 @@
                             <a class="text-white" href="#">sponsors</a>
                         </li>
                         <li class="md:mr-4 md:inline-block">
-                            <a class="btn-tickets inline-block w-full text-center text-white text-lg uppercase rounded-lg px-8 py-6 md:px-5 md:py-3"
+                            <a class="btn-tickets inline-block w-full text-center text-white uppercase rounded-lg px-8 py-6 md:px-5 md:py-3"
                                href="https://tickets.fullstackeurope.com">
                                 tickets
                             </a>

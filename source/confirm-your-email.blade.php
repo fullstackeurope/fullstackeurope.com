a55423c9151edcbf187f297e8e07dbd8b0335238
Dries Vints <dries.vints@gmail.com>
2019-02-01 09:55:18 +0100

    Newsletter pages

---
+++ source/confirm-your-email.blade.php
@@ -0,0 +1,12 @@
+@extends('_layouts.master')
+
+@section('body')
+    <div class="container mx-auto pt-32 max-w-md">
+        <h3 class="text-3xl text-center font-medium my-8">You’re almost there…</h3>
+
+        <p class="text-xl mb-16 leading-normal">
+            Thanks for signing up! There’s an e-mail on it’s way to your address for verification.
+            Just click the link to verify your address and you're all set.
+        </p>
+    </div>
+@endsection
---
+++ source/subscribed.blade.php
@@ -0,0 +1,12 @@
+@extends('_layouts.master')
+
+@section('body')
+    <div class="container mx-auto pt-32 max-w-md">
+        <h3 class="text-3xl text-center font-medium my-8">You are now subscribed!</h3>
+
+        <p class="text-xl mb-16 leading-normal">
+            Awesome! You are now fully signed up.
+            We'll let you know as soon as we have more details about the conference.
+        </p>
+    </div>
+@endsection

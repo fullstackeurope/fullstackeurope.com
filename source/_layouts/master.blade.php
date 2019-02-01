f53817bc9d1c95f5899e9356362dda9ba079cefd
Dries Vints <dries.vints@gmail.com>
2019-02-01 13:54:34 +0100

    Meta tags

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <title>Full Stack Europe</title>
-    <meta name="description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in 2019.">
 
+    @include('_layouts.meta')
     @include('_layouts.favicons')
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
---
+++ source/_layouts/meta.blade.php
@@ -0,0 +1,15 @@
+<meta name="description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in 2019.">
+
+<meta itemprop="name" content="Full Stack Europe">
+<meta itemprop="description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in 2019.">
+
+<meta name="twitter:card" content="summary">
+<meta name="twitter:title" content="Full Stack Europe">
+<meta name="twitter:description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in 2019.">
+<meta name="twitter:site" content="@fullstackeurope">
+
+<meta name="og:title" content="Full Stack Europe">
+<meta name="og:description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in 2019.">
+<meta name="og:url" content="https://fullstackeurope.com">
+<meta name="og:site_name" content="Full Stack Europe">
+<meta name="og:type" content="website">

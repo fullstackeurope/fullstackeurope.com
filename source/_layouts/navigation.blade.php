afb53485a2e69fb35098b48ae7448875f35d7cf0
Dries Vints <dries.vints@gmail.com>
2019-02-03 18:30:18 +0100

    Navigation adjustment

--- source/_layouts/navigation.blade.php
+++ source/_layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<div id="navigation" class="md:w-3/4 mt-10 md:mt-2 hidden md:block md:text-right">
+<div id="navigation" class="md:w-3/4 mt-10 md:mt-4 hidden md:block md:text-right">
     <nav>
         <ul class="list-reset">
             <li class="mb-6 md:mb-0 md:mr-6 md:inline-block">
@@ -8,7 +8,8 @@
                 <a class="text-white" href="#newsletter">newsletter</a>
             </li>
             <li class="md:mr-4 md:inline-block">
-                <a class="btn-tickets inline-block w-full text-center text-white uppercase rounded-lg px-8 py-6 md:px-5 md:py-3"
+                <a class="text-white hidden md:inline" href="{{ $page->ticketUrl }}">tickets</a>
+                <a class="btn-tickets inline-block md:hidden w-full text-center text-white uppercase rounded-lg px-8 py-6 md:px-5 md:py-3"
                    href="{{ $page->ticketUrl }}">
                     tickets
                 </a>

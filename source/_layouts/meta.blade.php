422aa094ef760b071aa471115ecb13ffd565cdd5
Dries Vints <dries.vints@gmail.com>
2019-02-03 18:20:32 +0100

    Add site image

--- source/_layouts/meta.blade.php
+++ source/_layouts/meta.blade.php
@@ -1,15 +1,19 @@
 <meta name="description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in 2019.">
+<meta name="image" content="/assets/images/preview.png">
 
 <meta itemprop="name" content="Full Stack Europe">
 <meta itemprop="description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in 2019.">
+<meta itemprop="image" content="/assets/images/preview.png">
 
 <meta name="twitter:card" content="summary">
 <meta name="twitter:title" content="Full Stack Europe">
 <meta name="twitter:description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in 2019.">
-<meta name="twitter:site" content="@fullstackeurope">
+<meta name="twitter:site" content="@fullstackeu">
+<meta name="twitter:image:src" content="/assets/images/preview.png">
 
 <meta name="og:title" content="Full Stack Europe">
 <meta name="og:description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in 2019.">
+<meta name="og:image" content="/assets/images/preview.png">
 <meta name="og:url" content="https://fullstackeurope.com">
 <meta name="og:site_name" content="Full Stack Europe">
 <meta name="og:type" content="website">

bbbef64004f474e55b956a210b01aed21913dfbb
Dries Vints <dries.vints@gmail.com>
2019-02-09 10:05:31 +0100

    Add On3Events logo

--- source/_partials/organisers.blade.php
+++ source/_partials/organisers.blade.php
@@ -2,7 +2,7 @@
     <h3 class="text-3xl font-medium text-center mb-8 md:mb-16">Organised by</h3>
 
     <div class="md:flex">
-        <div class="md:w-1/4 md:ml-auto text-center mb-8 md:mb-0">
+        <div class="md:w-1/5 md:ml-auto text-center mb-8 md:mb-0">
             <p class="mb-3">
                 <a href="https://twitter.com/freekmurze">
                     <img src="https://www.gravatar.com/avatar/a1b4b398630bbd32b77a82cbc37fae9a?s=200"
@@ -15,7 +15,7 @@
                 <a href="http://murze.be"><i class="fas fa-globe-africa"></i></a>
             </p>
         </div>
-        <div class="md:w-1/4 md:mr-auto text-center">
+        <div class="md:w-1/5 text-center mb-8 md:mb-0">
             <p class="mb-3">
                 <a href="https://twitter.com/driesvints">
                     <img src="https://www.gravatar.com/avatar/e8321183acdf47a9ce838afd13a964b5?s=200"
@@ -27,6 +27,13 @@
                 <a href="https://twitter.com/driesvints" class="mr-2"><i class="fab fa-twitter"></i></a>
                 <a href="https://driesvints.com"><i class="fas fa-globe-africa"></i></a>
             </p>
+        </div>
+        <div class="md:w-1/5 md:mr-auto text-center">
+            <p class="mb-3">
+                <img src="/assets/images/on3events.jpg"
+                     width="100" class="rounded-full inline-block">
+            </p>
+            <p class="mb-3">On3Events</p>
         </div>
     </div>
 </div>

8c394506872a6b757f9b0bf15af5aacc7101bf01
Dries Vints <dries.vints@gmail.com>
2019-02-16 12:04:08 +0100

    Add phone and email address

--- source/_layouts/footer.blade.php
+++ source/_layouts/footer.blade.php
@@ -6,12 +6,17 @@
                     <img src="/assets/images/logo.png" class="mb-4 md:mb-6" width="150" alt="Full Stack Europe"><br>
                     {{ $page->dates }} - {{ $page->venue }}
                 </div>
-                <div class="md:w-1/2 md:text-right">
+                <div class="relative md:w-1/2 md:text-right">
                     <ul class="list-reset">
                         <li data-scroll class="md:inline-block mb-4"><a href="#speakers">speakers</a></li>
                         <li data-scroll class="md:ml-6 md:inline-block mb-4"><a href="#newsletter">newsletter</a></li>
                         <li class="md:ml-6 md:inline-block mb-4"><a href="{{ $page->ticketUrl }}">tickets</a></li>
                     </ul>
+                    <div class="md:absolute text-center md:text-right pin-b pin-r mt-8">
+                        Need more help?<br>
+                        <a href="tel:+32-468-109891">+32-468-109891</a> |
+                        <a href="mailto:hello@fullstackeurope.com">hello@fullstackeurope.com</a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -26,9 +31,6 @@
                     Design by <a href="https://janvanechelpoel.be">Jan Van Echelpoel</a>
                 </div>
                 <div class="md:w-1/4 md:text-right text-lg">
-                    <a href="mailto:hello@fullstackeurope.com" class="mr-3">
-                        <i class="fas fa-envelope"></i>
-                    </a>
                     <a href="https://twitter.com/fullstackeu" class="mr-3">
                         <i class="fab fa-twitter"></i>
                     </a>

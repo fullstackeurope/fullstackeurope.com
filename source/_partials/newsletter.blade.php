27d1884c9009e767ef7e81c77a83464440be2d7c
Dries Vints <dries.vints@gmail.com>
2019-02-16 13:07:44 +0100

    Update newsletter component

--- source/_partials/newsletter.blade.php
+++ source/_partials/newsletter.blade.php
@@ -1,19 +1,19 @@
-<div id="newsletter" class="text-center text-sm leading-normal md:pt-16 pb-24" style="color: #818181; max-height: 400px;">
+<div id="newsletter" class="text-center text-sm leading-normal pb-12 md:pt-16 md:pb-24 style="color: #818181; max-height: 400px;">
     <div class="container py-4">
-        <div class="bg-white rounded shadow-lg px-4 md:px-10 py-16">
+        <div class="bg-white rounded shadow-lg px-4 py-6 md:py-12">
             <form action="https://sendy.murze.be/subscribe" method="post" accept-charset="utf-8">
                 <input type="hidden" name="list" value="iuUw3mYRnmw1tbGOPbNnpQ">
 
-                <div class="md:flex md:mb-4">
-                    <div class="md:w-1/4 mb-6 md:mb-0 lg:pt-2 xl:pt-0 md:pl-4 md:pr-4">
+                <div class="md:flex">
+                    <div class="md:w-1/3 lg:w-1/4 mb-6 md:mb-0 md:pt-1 xl:pt-0 md:pl-4 md:pr-4">
                         <h3 class="text-xl xl:text-2xl font-medium">Don't miss a thing</h3>
                         <p class="xl:text-lg">We won't spam you, promised</p>
                     </div>
                     <div class="md:w-1/2 mb-4 md:mb-0 md:pl-4">
                         <input type="email" name="email" class="inline-block text-center md:text-left text-lg w-full rounded px-8 py-6 md:h-16" placeholder="hello@fullstackeurope.com" required style="background-color: #F5F6FB; color: #989898;">
                     </div>
-                    <div class="md:w-1/4 md:pr-4">
-                        <button type="submit" class="btn-tickets inline-block w-full text-white text-lg uppercase font-medium rounded-lg px-8 py-6 md:h-16">
+                    <div class="md:w-1/6 lg:w-1/4 md:pr-4">
+                        <button type="submit" class="btn-tickets inline-block w-full text-white text-lg uppercase font-medium rounded-lg py-6 md:h-16">
                             Sign Up
                         </button>
                     </div>

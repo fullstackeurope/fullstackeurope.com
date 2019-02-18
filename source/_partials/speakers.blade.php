fb6147bd79f2597716ad2d99e4c8a347f1fb6576
Dries Vints <dries.vints@gmail.com>
2019-02-18 21:43:58 +0100

    Tweak speaker card spacing

--- source/_partials/speakers.blade.php
+++ source/_partials/speakers.blade.php
@@ -3,14 +3,14 @@
         <div id="speakers-inner" class="bg-white pt-10 md:pt-20 -mt-20">
             <h3 class="pre-title">Meet our speakers</h3>
             <h2 class="title">Featured talks from</h2>
-            <div class="md:flex mb-12">
-                <div class="md:w-1/3 md:px-4 lg:px-8 xl:px-12 mb-6 md:mb-0">
+            <div class="md:flex mb-12 md:px-4 lg:px-8 xl:px-10">
+                <div class="md:w-1/3 md:px-4 lg:px-8 xl:px-10 mb-6 md:mb-0">
                     @include('_partials._speaker', ['name' => 'John Doe', 'twitter' => 'johndoe'])
                 </div>
-                <div class="md:w-1/3 md:px-4 lg:px-8 xl:px-12 mb-6 md:mb-0">
+                <div class="md:w-1/3 md:px-4 lg:px-8 xl:px-10 mb-6 md:mb-0">
                     @include('_partials._speaker', ['name' => 'John Doe', 'twitter' => 'johndoe'])
                 </div>
-                <div class="md:w-1/3 md:px-4 lg:px-8 xl:px-12">
+                <div class="md:w-1/3 md:px-4 lg:px-8 xl:px-10">
                     @include('_partials._speaker', ['name' => 'John Doe', 'twitter' => 'johndoe'])
                 </div>
             </div>

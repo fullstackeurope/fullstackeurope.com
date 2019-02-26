10b99152e897bb0c47bc5af73c06771938ca50f2
Dries Vints <dries.vints@gmail.com>
2019-02-26 13:49:42 +0100

    Add Google Analytics

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,6 +1,19 @@
 <!DOCTYPE html>
 <html lang="en" class="h-full">
 <head>
+    @if ($page->production)
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
+        <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-123645073-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-123645073-1');
+        </script>
+    @endif
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 

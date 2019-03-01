766ac5df0d609812ddb88c5cddd95b4c563b58fd
Dries Vints <dries.vints@gmail.com>
2019-03-01 15:22:57 +0100

    Remove old newsletter pages

--- source/subscribed.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('_layouts.page')
-
-@section('content')
-    <h2 class="title">You are now subscribed!</h2>
-
-    <p class="paragraph">
-        Awesome! You are now fully signed up. You'll now receive updates about the conference.
-    </p>
-@endsection
--- source/unsubscribed.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('_layouts.page')
-
-@section('content')
-    <h2 class="title">You are now unsubscribed</h2>
-
-    <p class="paragraph">
-        We're sad to see you go. If you ever change your mind, just sign back up below.
-    </p>
-@endsection

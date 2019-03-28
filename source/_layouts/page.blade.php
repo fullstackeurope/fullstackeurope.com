7f4b4787ec568ca7ebfc453a9708167f49311ddb
Dries Vints <dries.vints@gmail.com>
2019-03-28 15:11:42 +0100

    Tweaks

--- source/_layouts/page.blade.php
+++ source/_layouts/page.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.master', ['bodyClass' => 'content-page'])
 
 @section('body')
-    <div id="page" class="container mx-auto pt-48 max-w-md text-grey-darker leading-normal">
+    <div id="page" class="container mx-auto pt-32 md:pt-theme-36 max-w-md text-grey-darker leading-normal mb-16">
         @yield('content')
     </div>
 @endsection

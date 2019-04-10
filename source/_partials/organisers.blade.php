76fdba1150e77426ff08e135a85e160a9a18a3f4
Dries Vints <dries.vints@gmail.com>
2019-04-10 14:08:49 +0200

    Organiser mobile tweaks

--- source/_partials/organisers.blade.php
+++ source/_partials/organisers.blade.php
@@ -1,8 +1,8 @@
 <div id="organisers" class="container text-grey-darker py-12 md:py-16">
-    <h2 class="title">Organised by</h2>
+    <h2 class="mb-16 title">Organised by</h2>
 
-    <div class="md:flex">
-        <div class="md:w-1/3 lg:w-1/5 md:ml-auto text-center mb-8 md:mb-0">
+    <div class="flex">
+        <div class="w-1/2 md:w-1/3 lg:w-1/5 ml-auto text-center mb-8 md:mb-0">
             <p class="mb-3">
                 <a href="https://twitter.com/freekmurze">
                     <img src="https://www.gravatar.com/avatar/a1b4b398630bbd32b77a82cbc37fae9a?s=200"
@@ -15,7 +15,7 @@
                 <a href="http://murze.be"><i class="fas fa-globe-africa"></i></a>
             </p>
         </div>
-        <div class="md:w-1/3 lg:w-1/5 md:mr-auto text-center mb-8 md:mb-0">
+        <div class="w-1/2 md:w-1/3 lg:w-1/5 mr-auto text-center mb-8 md:mb-0">
             <p class="mb-3">
                 <a href="https://twitter.com/driesvints">
                     <img src="https://www.gravatar.com/avatar/e8321183acdf47a9ce838afd13a964b5?s=200"

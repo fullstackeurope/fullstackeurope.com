07838121dc000d2952350ddfe4047322034c667c
Dries Vints <dries.vints@gmail.com>
2019-04-18 15:42:17 +0200

    Update bio Matthias

--- source/speakers/matthias-noback.blade.php
+++ source/speakers/matthias-noback.blade.php
@@ -1,11 +1,9 @@
 @extends('_layouts.speaker', ['speaker' => $page->speakers['matthias-noback']])
 
 @section('bio')
-    <p>Matthias is a professional PHP developer, skilled in most of the tried and proven web development languages, i.e. the big ones: PHP, MySQL, HTML, CSS and JavaScript. Amongst his favorite topics are clean code, unit testing, dependency injection and design principles &amp; patterns.</p>
+    <p>Matthias Noback is a professional web developer (since 2003). He lives in Zeist, The Netherlands, with his girlfriend, son, and daughter.</p>
 
-    <p>He has his own web development, training and consultancy company <a href="https://nobacksoffice.nl">Noback's Office</a>. In the past he has worked as a software developer at <a href="http://www.driebit.nl">Driebit</a> (Amsterdam) and <a href="http://www.ippz.nl">IPPZ</a> (Utrecht), and as CTO at <a href="https://www.ibuildings.nl">Ibuildings</a>.</p>
+    <p>Matthias has his own web development, training and consultancy company called Noback's Office. He has a strong focus on backend development and architecture, always looking for better ways to design software.</p>
 
-    <p>Since 2011 he has been regularly writing on this blog about advanced PHP and Symfony-related topics. He has also published at the <a href="https://www.ibuildings.nl/blog">Ibuildings blog</a>.</p>
-
-    <p>Matthias lives in Zeist, near Utrecht, in the Netherlands with his girlfriend, son and daughter.</p>
+    <p>Since 2011 he's been blogging about all sorts of programming-related topics on <a href="https://matthiasnoback.nl/">matthiasnoback.nl</a>. He's published several programming books as well (most recently: "Principles of Package Design" and a "Style Guide for Object Design").</p>
 @endsection

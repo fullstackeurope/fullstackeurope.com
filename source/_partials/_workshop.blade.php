f38ee1089377462a9c9c26ccd0c61ffebf738f9e
Dries Vints <dries.vints@gmail.com>
2019-05-14 16:12:25 +0200

    Responsive tweaks

--- source/_partials/_workshop.blade.php
+++ source/_partials/_workshop.blade.php
@@ -1,7 +1,7 @@
-<div class="flex mb-6">
-    <div class="w-1/3 md:w-1/6 text-center">
+<div class="md:flex mb-6">
+    <div class="md:w-1/6 text-center">
         <a href="/speakers/{{ $instructor['url'] }}">
-            <div class="speaker enlarge image-border-gradient-left inline-block shadow-lg-blue rounded h-24 md:w-32 overflow-hidden">
+            <div class="speaker enlarge image-border-gradient-left w-1/2 inline-block shadow-lg-blue rounded max-h-32 md:w-32 overflow-hidden">
                 <div style="height: inherit; transform: translateY(50%);">
                     <img src="/assets/images/speakers/{{ $instructor['url'] }}.jpg" alt="{{ $instructor['name'] }}"
                          class="block w-full" style="transform: translateY(-50%);">
@@ -9,22 +9,16 @@
             </div>
         </a>
     </div>
-    <div class="w-2/3 md:w-2/3 px-8 flex flex-col justify-center">
+    <div class="md:w-2/3 md:px-8 py-4 md:flex md:flex-col md:justify-center text-center md:text-left">
         <h3 class="font-noway-medium text-xl md:text-2xl mb-2"><a href="/speakers/{{ $instructor['url'] }}/#workshop">{{ $instructor['workshop'] }}</a></h3>
         <p class="mb-2">By {{ $instructor['name'] }} - {{ $instructor['bio'] }}</p>
     </div>
-    <div class="hidden md:block md:w-1/6 md:flex md:flex-col md:justify-center">
-        <a class="btn-tickets px-4 mb-3" href="{{ $page->ticketsUrl }}" target="_blank">Register now</a>
-        <p class="text-center">
+    <div class="md:w-1/6 md:flex md:flex-col md:justify-center">
+        <a class="btn-tickets px-4 mb-4 py-4" href="{{ $page->ticketsUrl }}" target="_blank">Register now</a>
+        <p class="md:text-center">
             <a href="/speakers/{{ $instructor['url'] }}/#workshop">
                 More info <span class="float-right md:float-none md:ml-4">&rsaquo;</span>
             </a>
         </p>
     </div>
 </div>
-
-<p class="md:hidden mb-6">
-    <a href="/speakers/{{ $instructor['url'] }}/#workshop">
-        More info <span class="float-right md:float-none md:ml-4">&rsaquo;</span>
-    </a>
-</p>

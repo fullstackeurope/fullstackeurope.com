08066152a39f6dc2303159dc3efd073bf8e008d3
Dries Vints <dries.vints@gmail.com>
2019-06-12 21:12:04 +0200

    Update talk

--- source/speakers/susanna-wong.blade.php
+++ source/speakers/susanna-wong.blade.php
@@ -1,6 +1,6 @@
 @extends('_layouts.speaker', ['speaker' => $page->speakers['susanna-wong']])
 
-@section('ta;l')
+@section('talk')
     <p>The launch of <a href="https://www.tensorflow.org/js">tensorflow.js</a> library in March 2018 has put the power of machine learning into the hands of the JavaScript developer, and with its specific capability to run machine learning programs entirely on the client-side, into the hands of the front-end dev. The talk documents the fundamental understanding of Machine Learning we need as a front end developer, in specifically the domain specific knowledge of setting up a Neural network with tensorflow.js, for us as web developers to integrate machine learning in our web projects.</p>
 
     <p>We will also explore possibilities of the architecture and design of static and dynamic ML systems in combination with existing front end frameworks (<a href="https://reactjs.org">React</a> and <a href="https://graphql.org">GraphQL</a>).</p>

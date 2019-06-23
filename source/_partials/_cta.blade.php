65dd6c85a324f682154af05128bff73e91dab414
freek <freek@spatie.be>
2019-06-23 21:38:07 +0200

    wip

--- source/_partials/_cta.blade.php
+++ source/_partials/_cta.blade.php
@@ -6,12 +6,9 @@
 @php($date = new DateTimeImmutable('2019-06-28 16:00', $timeZone))
 @php($timeRemaining = $date->diff(new DateTimeImmutable('now', $timeZone)))
 
-<p class="font-noway-medium italic text-sm leading-normal mb-8">
-    €130 discount with Early Bird Tickets
-</p>
 <p class="font-noway-medium italic text-2xl leading-normal">
     <span class="timer hidden" data-expires="{{ $date->getTimestamp() }}" data-threshold-days="30">
-        Early Birds still available for<br>
+        Early bird tickets, which offer a 130 EUR discount, are still available for<br>
         <span class="days">0</span>d,
         <span class="hours">0</span>h,
         <span class="minutes">0</span>m,

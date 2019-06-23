69ae4bfd12fe419a2f6642e26c26de64c4c1d055
Dries Vints <dries.vints@gmail.com>
2019-06-23 18:30:40 +0200

    Update timer

--- source/_partials/_cta.blade.php
+++ source/_partials/_cta.blade.php
@@ -3,13 +3,15 @@
 </a>
 
 @php($timeZone = new DateTimeZone('CET'))
-@php($date = new DateTimeImmutable('2019-07-01 16:00', $timeZone))
+@php($date = new DateTimeImmutable('2019-06-28 16:00', $timeZone))
 @php($timeRemaining = $date->diff(new DateTimeImmutable('now', $timeZone)))
 
-<p class="font-noway-medium italic text-sm leading-normal">
-    €130 discount with Early Bird Tickets<br>
+<p class="font-noway-medium italic text-sm leading-normal mb-8">
+    €130 discount with Early Bird Tickets
+</p>
+<p class="font-noway-medium italic text-2xl leading-normal">
     <span class="timer hidden" data-expires="{{ $date->getTimestamp() }}" data-threshold-days="30">
-        Still available for
+        Early Birds still available for<br>
         <span class="days">0</span>d,
         <span class="hours">0</span>h,
         <span class="minutes">0</span>m,

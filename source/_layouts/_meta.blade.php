62d32c01ebfdf4e35b1a7c0c041d919e4b3b67d2
Dries Vints <dries.vints@gmail.com>
2019-07-15 13:02:43 +0200

    Update meta data

--- source/_layouts/_meta.blade.php
+++ source/_layouts/_meta.blade.php
@@ -1,18 +1,21 @@
-<meta name="description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in October 2019.">
+@php($title = 'Full Stack Europe 2019 - The Full Stack Conference')
+@php($description = 'The international conference for every kind of developer. Come learn about Serverless, React, Machine Learning, Chatbots, JavaScript, Application Architecture, Microservices, Graph Databases, Varnish and much more!')
+
+<meta name="description" content="{{ $description }}">
 <meta name="image" content="/assets/images/preview.png">
 
-<meta itemprop="name" content="Full Stack Europe">
-<meta itemprop="description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in October 2019.">
+<meta itemprop="name" content="{{ $title }}">
+<meta itemprop="description" content="{{ $description }}">
 <meta itemprop="image" content="/assets/images/preview.png">
 
 <meta name="twitter:card" content="summary">
-<meta name="twitter:title" content="Full Stack Europe">
-<meta name="twitter:description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in October 2019.">
+<meta name="twitter:title" content="{{ $title }}">
+<meta name="twitter:description" content="{{ $description }}">
 <meta name="twitter:site" content="@fullstackeu">
 <meta name="twitter:image:src" content="/assets/images/preview.png">
 
-<meta name="og:title" content="Full Stack Europe">
-<meta name="og:description" content="Full Stack Europe is a conference for every kind of developer. First edition coming in October 2019.">
+<meta name="og:title" content="{{ $title }}">
+<meta name="og:description" content="{{ $description }}">
 <meta name="og:image" content="/assets/images/preview.png">
 <meta name="og:url" content="https://fullstackeurope.com">
 <meta name="og:site_name" content="Full Stack Europe">

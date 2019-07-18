86446be26d7f89289e805d0a19567eced980afde
Dries Vints <dries.vints@gmail.com>
2019-07-18 10:56:16 +0200

    Update cfp page

--- source/cfp.blade.php
+++ source/cfp.blade.php
@@ -1,5 +1,21 @@
 @extends('_layouts.page', ['pageTitle' => 'Lightning Talks CFP'])
 
 @section('content')
-    <iframe class="max-w-4xl my-20" src="https://docs.google.com/forms/d/e/1FAIpQLSfM_65JwOB8mFhLj1MsKIwmY_XxW3lsTpa4hEHVM5Lj6DDWYQ/viewform?embedded=true" width="100%" height="1894" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+    <h1>Lightning Talks CFP</h1>
+
+    <p>Full Stack Europe is the international conference for developers who want to learn across the stack. In between the regular talks we'll schedule lightning talks brought to you by our attendees. This way we want to give our attendees the opportunity to become a bigger part of the conference.</p>
+
+    <p><strong>Our lightning talks are 4 minutes long.</strong> We're looking for talks about any part of stack: front end, back end, ops, design and everything in between. Please don't hesitate to submit. Every attendee is welcome!</p>
+
+    <p>If you submit to the CFP, <strong>you’ll get a discount code that will enable you to buy a conference ticket with a discount of 50 EUR.</strong> If you get selected, <strong>you're also invited to join the speaker dinner.</strong> All lightning talk speakers must have a ticket to the conference.</p>
+
+    <p>To guarantee smooth transitions between talks you'll have to use our laptop to present your talk. <strong>Our laptop will contain Apple Keynote and Microsoft Powerpoint.</strong> When you get selected, you'll have to mail your slides to us ahead of the conference.</p>
+
+    <p>Use the button below to submit your idea. <strong>The deadline for lighting talks is the 10th of September.</strong></p>
+
+    <p>
+        <a class="btn-tickets mb-4 py-2 px-10" href="https://forms.gle/hCovBcsPQ712aBE37" target="_blank">
+            Submit Lightning Talk
+        </a>
+    </p>
 @endsection

f123cc8c19b98cf2e4e1e5490b14c6f88707a766
Dries Vints <dries.vints@gmail.com>
2019-07-18 10:28:40 +0200

    Page title

--- source/cfp.blade.php
+++ source/cfp.blade.php
@@ -1,4 +1,4 @@
-@extends('_layouts.page', ['pageTitle' => 'Code of Conduct'])
+@extends('_layouts.page', ['pageTitle' => 'Lightning Talks CFP'])
 
 @section('content')
     <iframe class="max-w-4xl my-20" src="https://docs.google.com/forms/d/e/1FAIpQLSfM_65JwOB8mFhLj1MsKIwmY_XxW3lsTpa4hEHVM5Lj6DDWYQ/viewform?embedded=true" width="100%" height="1894" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>

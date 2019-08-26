f4ec1d8d2be4a335fc3ecbcf4e3b6d0669d97956
Dries Vints <dries.vints@gmail.com>
2019-08-26 12:29:11 +0200

    Add countdown for workshop early bird

--- source/_partials/_cta.blade.php
+++ source/_partials/_cta.blade.php
@@ -3,12 +3,12 @@
 </a>
 
 @php($timeZone = new DateTimeZone('CET'))
-@php($date = new DateTimeImmutable('2019-07-01 16:00', $timeZone))
+@php($date = new DateTimeImmutable('2019-09-15 16:00', $timeZone))
 @php($timeRemaining = $date->diff(new DateTimeImmutable('now', $timeZone)))
 
-<p class="font-noway-medium italic text-2xl leading-normal">
-    <span class="timer hidden" data-expires="{{ $date->getTimestamp() }}" data-threshold-days="30">
-        Early bird tickets, which offer a €130 discount,<br>are still available for<br class="sm:hidden">
+<p class="font-noway-medium italic text-1xl leading-normal max-w-xs mx-auto">
+    <span class="timer" data-expires="{{ $date->getTimestamp() }}" data-threshold-days="30">
+        Early bird tickets for workshops, which offer a €100 discount, are still available for<br>
         <span class="days">0</span>d,
         <span class="hours">0</span>h,
         <span class="minutes">0</span>m,

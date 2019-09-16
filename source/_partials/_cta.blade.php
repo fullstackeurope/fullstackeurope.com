18b744d06c59a8cbf1224d6db8ca4e189f9098de
Dries Vints <dries.vints@gmail.com>
2019-09-16 12:42:35 +0200

    Disable timer

--- source/_partials/_cta.blade.php
+++ source/_partials/_cta.blade.php
@@ -6,20 +6,21 @@
 
 <p class="mb-4 sm:mb-10">
     <a class="text-white border-b border-dotted leading-normal" href="https://ti.to/on3/fullstack19">
-        Get a discount for teams of 4 <span class="whitespace-no-wrap">or more with Full Team Tickets!</span>
+        Get a discount for teams of 4<br>
+        or more with Full Team Tickets!
     </a>
 </p>
 
-@php($timeZone = new DateTimeZone('CET'))
-@php($date = new DateTimeImmutable('2019-09-15 16:00', $timeZone))
-@php($timeRemaining = $date->diff(new DateTimeImmutable('now', $timeZone)))
+{{--@php($timeZone = new DateTimeZone('CET'))--}}
+{{--@php($date = new DateTimeImmutable('2019-09-15 16:00', $timeZone))--}}
+{{--@php($timeRemaining = $date->diff(new DateTimeImmutable('now', $timeZone)))--}}
 
-<p class="font-noway-medium italic text-1xl leading-normal max-w-xs mx-auto">
-    <span class="timer" data-expires="{{ $date->getTimestamp() }}" data-threshold-days="30">
-        Early bird tickets for workshops, which offer a €100 discount, are still available for<br>
-        <span class="days">0</span>d,
-        <span class="hours">0</span>h,
-        <span class="minutes">0</span>m,
-        <span class="seconds">0</span>s
-    </span>
-</p>
+{{--<p class="font-noway-medium italic text-1xl leading-normal max-w-xs mx-auto">--}}
+{{--    <span class="timer" data-expires="{{ $date->getTimestamp() }}" data-threshold-days="30">--}}
+{{--        Early bird tickets for workshops, which offer a €100 discount, are still available for<br>--}}
+{{--        <span class="days">0</span>d,--}}
+{{--        <span class="hours">0</span>h,--}}
+{{--        <span class="minutes">0</span>m,--}}
+{{--        <span class="seconds">0</span>s--}}
+{{--    </span>--}}
+{{--</p>--}}

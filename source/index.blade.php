4766029cdac010843fcccb51f48bd1cff849e8ad
Dries Vints <dries.vints@gmail.com>
2019-09-25 17:14:50 +0200

    Add diversity charter

--- source/index.blade.php
+++ source/index.blade.php
@@ -50,4 +50,11 @@
 
     @include('_partials.sponsors')
     @include('_partials.organisers')
+
+    <div class="text-center">
+        <h2 class="mt-8 mb-16 title">The Diversity Charter</h2>
+        <a href="https://diversitycharter.org" target="_blank">
+            <img class="enlarge" src="/assets/images/diversitycharter.png" alt="Diversity Charter Logo">
+        </a>
+    </div>
 @endsection

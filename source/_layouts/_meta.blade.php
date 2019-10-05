493488e0b53d2a28ce6b82b2b89635ac9f258584
Dries Vints <dries.vints@gmail.com>
2019-10-05 16:11:30 +0200

    Use full paths for og imags

--- source/_layouts/_meta.blade.php
+++ source/_layouts/_meta.blade.php
@@ -2,21 +2,21 @@
 @php($description = 'The international conference for every kind of developer. Come learn about Serverless, React, Machine Learning, Chatbots, JavaScript, Application Architecture, Microservices, Graph Databases, Varnish and much more!')
 
 <meta name="description" content="{{ $description }}">
-<meta name="image" content="/assets/images/preview.png">
+<meta name="image" content="https://fullstackeurope.com/assets/images/preview.png">
 
 <meta itemprop="name" content="{{ $title }}">
 <meta itemprop="description" content="{{ $description }}">
-<meta itemprop="image" content="/assets/images/preview.png">
+<meta itemprop="image" content="https://fullstackeurope.com/assets/images/preview.png">
 
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:title" content="{{ $title }}">
 <meta name="twitter:description" content="{{ $description }}">
 <meta name="twitter:site" content="@fullstackeu">
-<meta name="twitter:image" content="/assets/images/preview.png">
+<meta name="twitter:image" content="https://fullstackeurope.com/assets/images/preview.png">
 
 <meta name="og:title" content="{{ $title }}">
 <meta name="og:description" content="{{ $description }}">
-<meta name="og:image" content="/assets/images/preview.png">
+<meta name="og:image" content="https://fullstackeurope.com/assets/images/preview.png">
 <meta name="og:url" content="https://fullstackeurope.com">
 <meta name="og:site_name" content="Full Stack Europe">
 <meta name="og:type" content="website">

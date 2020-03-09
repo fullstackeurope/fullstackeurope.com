501e19c619293406beab16ed3984ebee58adbc5e
Dries Vints <dries.vints@gmail.com>
2020-03-09 15:38:03 +0100

    Remove analytics

--- source/2020/_layouts/master.blade.php
+++ source/2020/_layouts/master.blade.php
@@ -1,20 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" class="h-full">
 <head>
-    @if ($page->production)
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-            ga('create', 'UA-123645073-1', 'auto', {'allowLinker': true});
-            ga('require', 'linker');
-            ga('linker:autoLink', ['fullstackeurope.com', 'ti.to']);
-            ga('send', 'pageview');
-        </script>
-    @endif
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -39,22 +25,6 @@
     <script src="{{ mix('js/main.js', '2020/assets/build') }}"></script>
 
     @if ($page->production)
-        <script type="text/javascript">
-            _linkedin_partner_id = "1228922";
-            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-        </script><script type="text/javascript">
-            (function(){var s = document.getElementsByTagName("script")[0];
-                var b = document.createElement("script");
-                b.type = "text/javascript";b.async = true;
-                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-                s.parentNode.insertBefore(b, s);})();
-        </script>
-        <noscript>
-            <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=1228922&fmt=gif" />
-        </noscript>
-
-        <!-- Fathom - simple website analytics - https://usefathom.com -->
         <script>
             (function(f, a, t, h, o, m){
                 a[h]=a[h]||function(){
@@ -68,7 +38,6 @@
             fathom('set', 'siteId', 'ZTTSULOQ');
             fathom('trackPageview');
         </script>
-        <!-- / Fathom -->
     @endif
 </body>
 </html>

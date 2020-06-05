1b76958fb8d819d567d07e783444058233705b26
Dries Vints <dries@vints.io>
2020-06-05 23:01:03 +0200

    Fix workshops block

--- resources/views/2019/partials/workshops.blade.php
+++ resources/views/2019/partials/workshops.blade.php
@@ -1,14 +1,16 @@
-<a id="workshops" class="anchor"></a>
-<div id="workshops-outer" class="container mx-auto px-6 bg-white text-gray-600 py-12 md:py-16">
-    <h3 class="pre-title">Learn by doing</h3>
-    <h2 class="title mb-4">Featured Workshops</h2>
-    <p class="text-center mb-16">Registrations: 8:30 &bull; Start: 9:30 &bull; End: 17:30</p>
+@if (count($workshops))
+    <a id="workshops" class="anchor"></a>
+    <div id="workshops-outer" class="container mx-auto px-6 bg-white text-gray-600 py-12 md:py-16">
+        <h3 class="pre-title">Learn by doing</h3>
+        <h2 class="title mb-4">Featured Workshops</h2>
+        <p class="text-center mb-16">Registrations: 8:30 &bull; Start: 9:30 &bull; End: 17:30</p>
 
-    @foreach ($workshops as $speaker)
-        @include('2019.partials.workshop', ['speaker' => $speaker])
+        @foreach ($workshops as $speaker)
+            @include('2019.partials.workshop', ['speaker' => $speaker])
 
-        @unless ($loop->last)
-            <hr class="border-t border-gray-100 my-6">
-        @endunless
-    @endforeach
-</div>
+            @unless ($loop->last)
+                <hr class="border-t border-gray-100 my-6">
+            @endunless
+        @endforeach
+    </div>
+@endif

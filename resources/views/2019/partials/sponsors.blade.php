c10ed50768d8f7bf678a9ea0faeed1dcb8973ec8
Dries Vints <dries@vints.io>
2020-06-05 22:59:25 +0200

    Fix sponsors block

--- resources/views/2019/partials/sponsors.blade.php
+++ resources/views/2019/partials/sponsors.blade.php
@@ -1,19 +1,19 @@
-<a id="sponsors" class="anchor-sponsors"></a>
-<div id="sponsors-outer" class="bg-gray-200 text-gray-600 py-12 md:py-16">
-    <div class="container mx-auto px-4">
-        <div class="md:flex text-center md:pb-0">
-            <div class="md:w-1/2">
-                <h3 class="pre-title md:text-left">Sponsors</h3>
-                <h2 class="title md:mb-0 md:text-left">Become Part Of The Stack</h2>
+@if (count($sponsors = $edition->sponsors))
+    <a id="sponsors" class="anchor-sponsors"></a>
+    <div id="sponsors-outer" class="bg-gray-200 text-gray-600 py-12 md:py-16">
+        <div class="container mx-auto px-4">
+            <div class="md:flex text-center md:pb-0">
+                <div class="md:w-1/2">
+                    <h3 class="pre-title md:text-left">Sponsors</h3>
+                    <h2 class="title md:mb-0 md:text-left">Become Part Of The Stack</h2>
+                </div>
+                <div class="md:w-1/2 text-left md:text-right text-lg md:pt-6">
+                    <a class="apply font-noway-medium block" href="mailto:hello@fullstackeurope.com?subject=We want to sponsor!">
+                        Apply as sponsor <span class="float-right md:float-none md:ml-4">&rsaquo;</span>
+                    </a>
+                </div>
             </div>
-            <div class="md:w-1/2 text-left md:text-right text-lg md:pt-6">
-                <a class="apply font-noway-medium block" href="mailto:hello@fullstackeurope.com?subject=We want to sponsor!">
-                    Apply as sponsor <span class="float-right md:float-none md:ml-4">&rsaquo;</span>
-                </a>
-            </div>
-        </div>
 
-        @if (count($sponsors = $edition->sponsors))
             @php($levels = $sponsors->groupBy('level'))
 
             @foreach (['platinum', 'gold', 'silver', 'bronze'] as $level)
@@ -29,6 +29,6 @@
                     </div>
                 @endif
             @endforeach
-        @endif
+        </div>
     </div>
-</div>
+@endif

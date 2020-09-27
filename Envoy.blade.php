9c245ea84ec2bd26766bb5346b2e6da3d2bad99d
Dries Vints <dries@vints.io>
2020-09-27 12:51:52 +0200

    Disable synx

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -115,7 +115,7 @@ php artisan storage:link
 {{ logMessage("✨  Optimizing installation…") }}
 cd {{ $newReleaseDir }};
 php artisan clear-compiled;
-php artisan schedule-monitor:sync
+{{--php artisan schedule-monitor:sync--}}
 @endtask
 
 @task('backupDatabase', ['on' => 'remote'])

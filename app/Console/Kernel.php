6ef3ae827b6eeddad458ccf2dbbafaf4ef2e6660
Dries Vints <dries@vints.io>
2020-09-30 21:22:51 +0200

    Add horizon snapshot command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('schedule-monitor:clean')->daily();
         $schedule->command('backup:clean')->daily()->at('01:00');
         $schedule->command('backup:run')->daily()->at('02:00');
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
     /**

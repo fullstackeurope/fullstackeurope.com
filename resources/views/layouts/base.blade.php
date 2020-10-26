94137111ab4ed7c9ec585f91b441505316c258f9
Dries Vints <dries@vints.io>
2020-10-26 22:29:15 +0100

    Update fathom

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -11,28 +11,18 @@
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ mix("css/{$edition->year}/app.css") }}">
+
+    @production
+        <!-- Fathom - beautiful, simple website analytics -->
+        <script src="https://boom.laravel.io/script.js" data-site="ZTTSULOQ" defer></script>
+        <!-- / Fathom -->
+    @endproduction
 </head>
 <body class="{{ $bodyClass ?? '' }}">
 
 @yield('body')
 
 <script src="{{ mix("js/{$edition->year}/app.js") }}"></script>
-
-@production
-    <script>
-        (function(f, a, t, h, o, m){
-            a[h]=a[h]||function(){
-                (a[h].q=a[h].q||[]).push(arguments)
-            };
-            o=f.createElement('script'),
-                m=f.getElementsByTagName('script')[0];
-            o.async=1; o.src=t; o.id='fathom-script';
-            m.parentNode.insertBefore(o,m)
-        })(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
-        fathom('set', 'siteId', 'ZTTSULOQ');
-        fathom('trackPageview');
-    </script>
-@endproduction
 
 </body>
 </html>

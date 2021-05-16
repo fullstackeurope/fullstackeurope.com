83c111284bcf65136a69106b6570a1bfeeb839ab
Rias <hey@rias.be>
2021-05-16 15:22:21 +0200

    Add code formatting through github actions

--- resources/2019/tailwind.config.js
+++ resources/2019/tailwind.config.js
@@ -1,29 +1,34 @@
 module.exports = {
-    purge: ['./**/*.php', './**/*.js'],
+    purge: ["./**/*.php", "./**/*.js"],
     theme: {
         extend: {
             colors: {
-                primary: '#6859ea',
-                secondary: '#6acbe0',
-                light: '#e5e3fe',
-                background: '#20195B',
-                'background-secondary': '#2D256F',
+                primary: "#6859ea",
+                secondary: "#6acbe0",
+                light: "#e5e3fe",
+                background: "#20195B",
+                "background-secondary": "#2D256F",
 
-                'theme-grey-light': '#E9EDF6',
+                "theme-grey-light": "#E9EDF6",
             },
             fontFamily: {
-                noway: ['noway', 'Helvetica', 'Arial', 'sans-serif'],
-                'noway-medium': ['nowaymedium', 'Helvetica', 'Arial', 'sans-serif'],
+                noway: ["noway", "Helvetica", "Arial", "sans-serif"],
+                "noway-medium": [
+                    "nowaymedium",
+                    "Helvetica",
+                    "Arial",
+                    "sans-serif",
+                ],
             },
             boxShadow: {
-                'lg-blue': '0 5px 15px 0 rgba(104, 89, 234, .1)',
+                "lg-blue": "0 5px 15px 0 rgba(104, 89, 234, .1)",
             },
         },
         linearGradientColors: {
-            primary: ['#6acbe0 0%', '#6859ea 50%', '#6acbe0 100%'],
-            secondary: ['#6acbe0 0%', '#6859ea 100%'],
+            primary: ["#6acbe0 0%", "#6859ea 50%", "#6acbe0 100%"],
+            secondary: ["#6acbe0 0%", "#6859ea 100%"],
         },
     },
     variants: {},
-    plugins: [require('tailwindcss-gradients')],
+    plugins: [require("tailwindcss-gradients")],
 };
--- resources/2021/webpack.mix.js
+++ resources/2021/webpack.mix.js
@@ -1,8 +1,5 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-mix
-    .setPublicPath('../../public/2021')
-    .js('js/app.js', 'app.js')
-    .postCss('css/app.css', 'app.css', [
-        require('tailwindcss'),
-    ]);
+mix.setPublicPath("../../public/2021")
+    .js("js/app.js", "app.js")
+    .postCss("css/app.css", "app.css", [require("tailwindcss")]);

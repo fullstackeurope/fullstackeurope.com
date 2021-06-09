4d8b04d352ac6d498e00b83ba8801bf0cd6b8789
Dries Vints <dries@vints.io>
2021-06-09 10:26:25 +0200

    Try versioning

--- resources/2021/webpack.mix.js
+++ resources/2021/webpack.mix.js
@@ -2,4 +2,5 @@ const mix = require('laravel-mix');
 
 mix.setPublicPath('../../public/assets/2021')
     .js('js/app.js', 'app.js')
-    .postCss('css/app.css', 'app.css', [require('tailwindcss')]);
+    .postCss('css/app.css', 'app.css', [require('tailwindcss')])
+    .version();
--- resources/2022/webpack.mix.js
+++ resources/2022/webpack.mix.js
@@ -2,4 +2,5 @@ const mix = require('laravel-mix');
 
 mix.setPublicPath('../../public/assets/2022')
     .js('js/app.js', 'app.js')
-    .postCss('css/app.css', 'app.css', [require('tailwindcss')]);
+    .postCss('css/app.css', 'app.css', [require('tailwindcss')])
+    .version();

9cc1becc11634251c6fa66d4f065b9bbfab290bf
Rias <hey@rias.be>
2021-06-24 13:36:36 +0200

    Fix artwork

--- resources/2021/views/partials/artwork.blade.php
+++ resources/2021/views/partials/artwork.blade.php
@@ -3,8 +3,10 @@
 @section('body')
     <div id="artwork" class="mx-auto p-12 leading-relaxed flex flex-col" style="width: 1200px; height: 630px; background: url({{ asset('assets/2021/images/pattern.png') }}) repeat">
         <div class="flex justify-between mb-12">
+            <div class="w-40 h-40 rounded-full bg-white overflow-hidden">
+                <img class="h-full w-full object-cover" src="{{ $avatar }}" alt="">
+            </div>
             <img class="w-1/3" src="{{ asset('assets/2021/images/logo-black.svg') }}" alt="">
-            <img class="rounded-full w-32 h-32 bg-white" src="{{ $avatar }}" alt="">
         </div>
         <div>
             <h1 class="text-5xl font-bold mb-3">{{ $speaker_name }}: {{ $title }}</h1>

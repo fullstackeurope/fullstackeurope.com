8f15170e29d63ad7ed903d7e404e2a6903f131fa
riasvdv <hey@rias.be>
2022-03-30 18:15:45 +0200

    Remove old edition front-end source files

--- resources/2019/webpack.mix.js
+++
@@ -1,6 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.setPublicPath('../../public/assets/2019')
-    .js('js/app.js', 'app.js')
-    .postCss('css/app.css', 'app.css', [require('tailwindcss')])
-    .version();
--- resources/2021/webpack.mix.js
+++
@@ -1,6 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.setPublicPath('../../public/assets/2021')
-    .js('js/app.js', 'app.js')
-    .postCss('css/app.css', 'app.css', [require('tailwindcss')])
-    .version();

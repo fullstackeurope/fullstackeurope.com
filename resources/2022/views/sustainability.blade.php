a64c2ce3d50975d09ff1646c42cc7d60d7c0cada
Rias <hey@rias.be>
2022-05-04 09:47:15 +0200

    update sustainability

--- resources/2022/views/sustainability.blade.php
+++ resources/2022/views/sustainability.blade.php
@@ -11,7 +11,7 @@
         <li>We're a remote-first company, and use public transport where possible.</li>
         <li>We book travel for our speakers so we can offer more sustainable alternatives (such as trains) instead of flights.</li>
         <li>We carbon offsett the flights we book.</li>
-        <li>We encourage our participants to come by train if possible. Amsterdam has good train connections to Brussels, London, Paris,...</li>
+        <li>We encourage our participants to come by train if possible.</li>
     </ul>
 
     <h2>Venue</h2>
@@ -27,20 +27,9 @@
         <li>We closely estimate our food amount through a careful headcount to minimise waste. We create awareness with our attendees to not throw away food. We are looking into the options of donating leftover food.</li>
         <li>Drinks are offered in porcelain and glassware. We provide water bottles with tap water.</li>
         <li>We provide bio straws (we can offer plastic straws for those who need it for accessibility reasons).</li>
-        <li>We discussed thoroughly with our caterer the use of reusable plates. However, for this year, we are not able to provide all of our food on reusable plates. Where needed, we will use 100% biodegradable disposables that compost within 90 days and therefore meet the EN13432 standard. Of course, the sustainable use of disposables depends on a good collection and recycling.</li>
     </ul>
 
     <h2>Recycling</h2>
 
     <p>During the conference we optimize waste processing and recycling. We have separate trash cans with bio compostable garbage bags, for all compostables. We also have separate paper bins.</p>
-
-    <h2>Logistics</h2>
-
-    <ul>
-        <li>Our totebags are fair trade and made of recycled bio cotton.</li>
-        <li>Our T-shirts are 100% bio cotton.</li>
-        <li>We work with suppliers that print without chemical ink.</li>
-        <li>We wash reuse materials such as staff shirts, lanyards, ...</li>
-        <li>We order recycled paper: stickie notes, program booklets, ...</li>
-    </ul>
 @endsection

80213a22071c18f2c0626646a6eae99968da2769
Rias <hey@rias.be>
2022-10-23 10:25:42 +0200

    Remove old resources

--- resources/2019/views/partials/diversity.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="text-center">
-    <h2 class="mt-8 mb-16 title">We Support the Diversity Charter</h2>
-    <a href="https://diversitycharter.org" target="_blank">
-        <img class="enlarge mx-auto" src="{{ asset('/assets/2019/images/diversitycharter.png')  }}" alt="Diversity Charter Logo">
-    </a>
-</div>
--- resources/2021/views/partials/cta_workshop.blade.php
+++
@@ -1,5 +0,0 @@
-<p class="text-center mt-8">
-    <span class="btn-tickets mb-4 py-2 px-10">
-        Tickets closed
-    </span>
-</p>

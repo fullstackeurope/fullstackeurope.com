4ad835aa49fd6d0ad4cc71f5ceabc5ef0455e10b
Dries Vints <dries@vints.be>
2023-06-02 21:35:50 +0200

    4 speakers on a row

--- resources/2023/views/partials/talks.blade.php
+++ resources/2023/views/partials/talks.blade.php
@@ -1,8 +1,8 @@
 @if (count($speakers))
-    <div id="speakers" class="container mx-auto px-4 bg-white mx-auto pt-10 pb-10 md:pt-20">
+    <div id="speakers" class="container mx-auto px-4 bg-white pt-10 pb-10 md:pt-20">
         <h3 class="pre-title">Meet our speakers</h3>
         <h2 class="title">Featuring talks from</h2>
-        <div class="md:grid md:grid-cols-3 md:col-gap-16 md:row-gap-12 md:px-4 lg:px-8 xl:px-16">
+        <div class="md:grid md:grid-cols-3 lg:grid-cols-4 md:col-gap-6 md:row-gap-6 md:px-4 lg:px-6">
             @foreach ($speakers as $speaker)
                 @continue ($speaker->talks->count() === 0 && $speaker->workshops->count())
                 @include('2023::partials.talk', compact('speaker'))

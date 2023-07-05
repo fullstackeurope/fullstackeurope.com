23cbd035b63913b083098df3f96f80d52eadc7c8
Dries Vints <dries@vints.be>
2023-07-05 20:24:23 +0200

    wip

--- resources/2023/views/cfp.blade.php
+++ resources/2023/views/cfp.blade.php
@@ -10,7 +10,7 @@
     <h2>Quick Summary</h2>
 
     <ul>
-        <li>The conference offers 30min presentations and full or half day workshops that discuss technical or non-technical topics across the stack.</li>
+        <li>The conference offers 25min or 50min presentations and full or half day workshops that discuss technical or non-technical topics across the stack.</li>
         <li>Talks are focussed on web development (mostly technical ones) but we're open to other related topics around software design in general as well.</li>
         <li>We only accept lightning talks from people who are already attending the conference.</li>
     </ul>
@@ -37,11 +37,11 @@
 
     <p>If you have already bought a ticket, and your proposal is accepted, we'll fully refund it. Or you can give it away to a friend or colleague.</p>
 
-    <div class="text-center mt-12">
+    {{-- <div class="text-center mt-12">
         <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSdgQjf39QepP07gLE88g7-Cn7Xdfast_mHVSUp25CfpjAM2uw/viewform">
             <button class="btn-tickets inline-block text-white text-lg uppercase font-noway-medium rounded-lg px-0 lg:px-12 py-5 md:h-16">
                 Apply to speak
             </button>
         </a>
-    </div>
+    </div> --}}
 @endsection

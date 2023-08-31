c73f45c1cd3c90de5e580e8c328b223d70813988
Rias <hey@rias.be>
2023-08-31 19:16:09 +0200

    add discount

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('discount', 'https://ti.to/on3/fseu23/discount/10');
+Route::redirect('2023/discount', 'https://ti.to/on3/fseu23/discount/10');
 
 Route::redirect('/login', '/nova/login')->name('login');
 

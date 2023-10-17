667f5623bc3cbd79cc86399f385a926121131f2c
Rias <hey@rias.be>
2023-10-17 10:52:07 +0200

    update copy

--- resources/2023/views/partials/alert.blade.php
+++ resources/2023/views/partials/alert.blade.php
@@ -3,9 +3,9 @@
         <div class="bg-white rounded shadow-lg px-4 py-6 md:py-12">
             <div class="mb-4 max-w-2xl mx-auto text-base leading-normal">
                 <h3 class="text-xl xl:text-2xl font-noway-medium mb-6">Important information</h3>
-                <p class="mb-4">Last night, before the football match between Sweden and Belgium, a terrorist incident happened in the city of Brussels. The suspect has been apprehended by the Belgian police.</p>
+                <p class="mb-4">Last night, before the football match between Sweden and Belgium, a terrorist shot three Swedish fans, as a reaction to a Quran burning in Sweden. The suspect has been apprehended by the Belgian police.</p>
                 <p class="mb-4">Belgium has raised the security threat level from 2 to 3. Right now, air and train travel is not affected. The Belgian and the Swedish governments both recommend to be vigilant. They did not issue a warning against traveling, as they do not see a heightened risk.</p>
-                <p class="mb-4">Therefore, we are proceeding with the conference as planned and there's no reason for concern.</p>
+                <p class="mb-4">Therefore, we are proceeding with the conference as planned, and hope that you will join as well.</p>
                 <p class="mb-4">Please be aware that a lot of misinformation regarding the incident is being spread on social media. We advise to only consult official sources.</p>
                 <p class="mb-4">If you do feel worried or have any questions, please do not hesitate to contact us via <a
                         href="mailto:hello@fullstackeurope.com">hello@fullstackeurope.com</a> or <a href="tel:+32468109891">+32 468 109891</a></p>
